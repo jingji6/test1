fc05ec65d1c1dca882e781a2e8ac2c8d4e82ccf2
JingJi <1726072727@qq.com>
2020-07-08 10:40:15 +0800

    第一次向github提交代码

---
+++ 1.py
@@ -0,0 +1,72 @@
+# #字符串
+# print("你好！")
+# #数字  
+# print(23333)
+# #小数
+# print(2.333)
+# #布尔值
+# print(True or False)
+# #元组
+# print(())
+# #数组
+# print([])
+# #字典
+# print({})
+# #注释
+# 'haha'
+# '''
+# 锄禾日当午
+# 汗滴禾下土
+# '''
+# #打印多个字符串
+# print("哈哈",2333)
+# #字符串拼接
+# print('哈哈'+'嘻嘻')
+# #运算符（+-*\）
+# print('哈哈'*10)
+# print((2+1)*10-9)
+
+
+#input方法
+# #变量
+# #赋值
+
+# a = "张三"  #把张三这个值赋值给了名字叫a的这个变量
+# print(a) 
+
+# a = input("请输入：")
+# print("input获取的数据：",a)
+
+#type方法 数据类型
+# print(type("2333"))
+# print(type(2333))
+# print(type(23.33))
+# print(type(True))
+# print(type(()))
+# print(type([]))
+# print(type({}))
+
+
+#转换数据类型
+# print(type(int(2.33)))
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("a+b=",a+b)
+
+
+
+#len()方法 长度
+# a = "1.2333333333333"
+# print(len(a))
+
+#练习：通过代码获取两段内容，并且计算他们的长度
+#1：
+# a = len(input("请输入："))
+# b = len(input("请输入："))
+# print("a+b=",a+b)
+
+#2：
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符的长度：",len(a)+len(b))
